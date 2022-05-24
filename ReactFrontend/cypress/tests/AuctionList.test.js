bb7489519fb56e7cb7a6a1b49bce25d0f76a7671
Benjamin Berglund <benjaminberglund@gmail.com>
2022-05-25 00:17:10 +0200

    getting data from fixture

--- ReactFrontend/cypress/tests/AuctionList.test.js
+++ ReactFrontend/cypress/tests/AuctionList.test.js
@@ -1,14 +1,14 @@
 
 describe("AuctionList", ()=>{
-
+    
     it("Lists matching results when I search", ()=>{        
         cy.visit("/")
-        // cy.fixture('example.json').as('fixData')
-        let terms = ["chili", "cotton", "red", "pepper"]
-        for(let term of terms){
-            cy.get('.search-input').clear().type(term)
-            cy.get('.card-title').contains(term, { matchCase: false })
-        }        
+        cy.fixture('example.json').as('data').then((data)=>{            
+            for(let term of data.searchTerms){ // ["chili", "cotton", "red", "pepper"]
+                cy.get('.search-input').clear().type(term)
+                cy.get('.card-title').contains(term, { matchCase: false })
+            } 
+        })               
     })
 
 })
